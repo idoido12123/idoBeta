6b457338bafeb15efe6253715cd2cfc727a35aea
idoido12123 <ak6361@bs.amalnet.k12.il>
2020-04-12 18:49:14 +0300

    push5

--- app/src/main/java/com/example/idobeta/ChosenList.java
+++ app/src/main/java/com/example/idobeta/ChosenList.java
@@ -47,9 +47,11 @@ public class ChosenList extends AppCompatActivity implements AdapterView.OnItemL
     EditText productName;
     EditText productAmount;
     EditText orderName;
+    EditText newAmount;
     ArrayList<NewList>shopValues=new ArrayList<>();
     ArrayList<NewList>listsHelper=new ArrayList<>();
     ArrayList<Product>productsHelper=new ArrayList<>();
+    int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,6 +99,7 @@ public class ChosenList extends AppCompatActivity implements AdapterView.OnItemL
        query.addValueEventListener(products1);
         products.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         products.setOnItemLongClickListener(this);
+        products.setOnItemClickListener(this);
 }
     public void newProduct(View view) {
         productDialog = (LinearLayout) getLayoutInflater().inflate(R.layout.add_product, null);
@@ -192,14 +195,59 @@ public class ChosenList extends AppCompatActivity implements AdapterView.OnItemL
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        position=i;
         changeAmountDialog=(LinearLayout) getLayoutInflater().inflate(R.layout.new_amount,null);
-        productName = (EditText) productDialog.findViewById(R.id.productName);
-        productAmount = (EditText) productDialog.findViewById(R.id.productAmount);
-        orderName = (EditText) productDialog.findViewById(R.id.orderName);
-        addProduct1 = new AlertDialog.Builder(this);
-        addProduct1.setView(productDialog);
-        addProduct1.setTitle("add new product");
-        addProduct1.setPositiveButton("OK", OKclick1);
-        addProduct1.show();
+        newAmount = (EditText) changeAmountDialog.findViewById(R.id.newAmount);
+        changeAmount = new AlertDialog.Builder(this);
+        changeAmount.setView(changeAmountDialog);
+        changeAmount.setTitle("change amount");
+        changeAmount.setPositiveButton("OK", OKclick2);
+        changeAmount.show();
     }
+    DialogInterface.OnClickListener OKclick2 = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, final int i) {
+            if (i == DialogInterface.BUTTON_POSITIVE) {
+                final String newAmount1 = newAmount.getText().toString();
+                Query query = refFamily.orderByChild("familyUname").equalTo(familyName);
+                ValueEventListener Namount=new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        shopValues.clear();
+                        productsValues.clear();
+                        listsHelper.clear();
+                        productsHelper.clear();
+                        for (DataSnapshot data : dataSnapshot.getChildren()) {
+                            Family family = data.getValue(Family.class);
+                            shopValues=family.getLists();
+                            while (!shopValues.isEmpty()){
+                                NewList list=shopValues.remove(0);
+                                if(list.getListName().equals(lName)) {
+                                    while (!list.getProducts().isEmpty()) {
+                                        Product product = list.getProducts().remove(0);
+                                        String str = product.getNproduct() + " * " + product.getCamut() + " , order : " + product.getNorder();
+                                        if (!str.equals(products.getItemAtPosition(position))) {
+                                            productsHelper.add(product);
+                                        }
+                                        else {
+                                            product.setCamut(newAmount1);
+                                            productsHelper.add(product);
+                                        }
+                                    }
+                                    list.setProducts(productsHelper);
+                                }
+                                listsHelper.add(list);
+                            }
+                            family.setLists(listsHelper);
+                            refFamily.child(familyName).setValue(family);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                };
+                query.addListenerForSingleValueEvent(Namount);
+            }
+        }
+    };
 }
